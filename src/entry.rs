97419262cff99b9f49c038c041dca82b9c9bc5b8
LiosK <contact@mail.liosk.net>
2023-03-24 08:37:21 +0900

    docs: fix inconsistent wording

--- src/entry.rs
+++ src/entry.rs
@@ -131,7 +131,7 @@ mod tests_v7 {
         for _ in 0..10_000 {
             let ts_now = (time::SystemTime::now()
                 .duration_since(time::UNIX_EPOCH)
-                .expect("clock may have gone backward")
+                .expect("clock may have gone backwards")
                 .as_millis()) as i64;
             let mut timestamp = 0i64;
             for e in uuid7().as_bytes().iter().take(6) {
